dfe1fce36d9c79006035c3387a94e7c194598c52
iPolarisu <elpolarisu@gmail.com>
2023-04-23 12:31:17 -0400

    chore: update config file

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "---";   // your network SSID (name)
+#define WIFI_PSK "---";   // your network password
+#define BROKER_IP "---"; // mqtt broker ip, usually rpi
+#define BROKER_PORT "---"; // mqtt broker port, usually 1883
+#define ESP_NAME "---"; // define name for esp (should not collide with others on the network)
+#define DOOR_TOPIC "---"; // define door topic name

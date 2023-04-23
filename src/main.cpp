f556be393043b577cda119610a9bd40cbc42c6e1
iPolarisu <elpolarisu@gmail.com>
2023-04-23 12:09:16 -0400

    feat: add door opening example

--- src/main.cpp
+++ src/main.cpp
@@ -1,147 +1,115 @@
 #include <Arduino.h>
-/*
- * in file secrets.h you have to define:
- *
- * #define WIFI_SSID "your wifi ssid";   // your network SSID (name)
- * #define WIFI_PSK "your wifi pass";   // your network password
- *
- */
-
-#include "secrets.h"
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
+#include "config.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define I2C_SDA 21
-#define I2C_SCL 22
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme; // I2C
-
-unsigned long delayTime = 10000;
-
-#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 900 /* Time ESP32 will go to sleep (in seconds) */
-
+// setting clients
 WiFiClient espClient;
-
-float temperatureC;
-float pressure;
-float humidity;
-float altitude;
-
-const char* ssid = WIFI_SSID;   // your network SSID (name)
-const char* password = WIFI_PSK;   // your network password
-
-const char* mqtt_server = "192.168.1.135"; // ip of rpi with mqtt broker running
 PubSubClient client(espClient);
-const int lamp = LED_BUILTIN;
 
-// Timers auxiliar variables
-long now = millis();
-long lastMeasure = 0;
+// setting config from config.h
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PSK;
+const char* mqtt_server = BROKER_IP;
+const char* mqtt_port = BROKER_PORT;
+const char* esp_name = ESP_NAME;
+const char* door_topic = DOOR_TOPIC;
 
+// callback to be called when posting to topic
 void callback(String topic, byte* message, unsigned int length);
+
+// esp led for testing purposes
+const int lamp = LED_BUILTIN;
 
 void setup() {
 
+  // start serial
   Serial.begin(115200);
   delay(100);
-  Serial.println(F("BME280 test"));
-
-  bool status;
-
-  pinMode(lamp,OUTPUT);
-  digitalWrite(lamp,HIGH);
-
-  WiFi.mode(WIFI_STA);
-
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
-
-  Serial.println("-- Default Test --");
-  delayTime = 1000;
-
   Serial.println();
 
-  // Connect or reconnect to WiFi
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect");
-    while (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, password);
-      delay(5000);
-    }
-    Serial.println("\nConnected.");
-  }
+  // setting pinmode for lamp
+  pinMode(lamp, OUTPUT);
+  digitalWrite(lamp, HIGH);
+ 
+  // set wifi to station mode (join a network that already exists)
+  WiFi.mode(WIFI_STA);
 
-  client.setServer(mqtt_server, 1883);
+  // try to connect to network
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println("...");
+ 
+  // 20 seconds timeout
+  while(WiFi.status()!=WL_CONNECTED && retries<20)
+  {
+    Serial.print(".");
+    retries++;
+    delay(1000);
+  }
+ 
+  Serial.println(); 
+  
+  // if timeout, print it
+  if(retries==20)
+  {
+    Serial.print("Unable to Connect to ");
+    Serial.println(ssid);
+  }
+  
+  // connected to wifi
+  if(WiFi.status()==WL_CONNECTED)
+  {
+    Serial.print("Successfully connected to ");
+    Serial.println(ssid);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  }
+  
+  // set mqtt broker and callback
+  client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
 }
 
-// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
-// Change the function below to add logic to your program, so when a device publishes a message to a topic that
-// your ESP8266 is subscribed you can actually do something
+// callback for message on suscribed topic
 void callback(String topic, byte* message, unsigned int length) {
+
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
-  String messageTemp;
+  String messageTopic;
 
   for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
+    messageTopic += (char)message[i];
   }
   Serial.println();
-  // Feel free to add more if statements to control more GPIOs with MQTT
 
-  // If a message is received on the topic fab/lamp, you check if the message is either on or off. Turns the lamp GPIO according to the message
-  if(topic=="fab/lamp"){
-      Serial.print("Changing Room lamp to ");
-      if(messageTemp == "on"){
+  // message received on door topic
+  if(topic == door_topic){
+      
+      // open door
+      if(messageTopic == "open"){
         digitalWrite(lamp, LOW);
-        Serial.print("On");
-      }
-      else if(messageTemp == "off"){
-        digitalWrite(lamp, HIGH);
-        Serial.print("Off");
+        Serial.print("Opening door");
       }
   }
+
   Serial.println();
 }
 
-// This functions reconnects your ESP8266 to your MQTT broker
-// Change the function below if you want to subscribe to more topics with your ESP8266
 void reconnect() {
+
   // Loop until we're reconnected
   while (!client.connected()) {
+    
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    /*
-     YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
-     To change the ESP device ID, you will have to give a new name to the ESP8266.
-     Here's how it looks:
-       if (client.connect("ESP8266Client")) {
-     You can do it like this:
-       if (client.connect("ESP1_Office")) {
-     Then, for the other ESP:
-       if (client.connect("ESP2_Garage")) {
-      That should solve your MQTT multiple connections problem
-    */
-    if (client.connect("ESP8266Client")) {
+
+    if (client.connect(esp_name)) {
       Serial.println("connected");
-      // Subscribe or resubscribe to a topic
-      // You can subscribe to more topics (to control more LEDs in this example)
-      client.subscribe("fab/lamp");
+      client.subscribe(door_topic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -150,69 +118,18 @@ void reconnect() {
       delay(5000);
     }
   }
+
 }
-
-void getValues() {
-  temperatureC = bme.readTemperature();
-  humidity = bme.readHumidity();
-  pressure = bme.readPressure() / 100.0F;
-  altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-}
-
-void printValues() {
-  Serial.print("Temperature = ");
-  Serial.print(temperatureC);
-  Serial.println(" °C");
-
-  // Convert temperature to Fahrenheit
-  /*Serial.print("Temperature = ");
-    Serial.print(1.8 * bme.readTemperature() + 32);
-    Serial.println(" *F");*/
-
-  Serial.print("Pressure = ");
-  Serial.print(pressure);
-  Serial.println(" hPa");
-
-  Serial.print("Approx. Altitude = ");
-  Serial.print(altitude);
-  Serial.println(" m");
-
-  Serial.print("Humidity = ");
-  Serial.print(humidity);
-  Serial.println(" %");
-
-  Serial.println();
-}
-
 
 void loop() {
 
-  delay(delayTime);
+  delay(5000);
 
   if (!client.connected()) {
     reconnect();
   }
+
   if(!client.loop())
-    client.connect("ESP8266Client");
-
-  now = millis();
-  // Publishes new temperature and humidity every 30 seconds
-  if (now - lastMeasure > 300000) {
-    lastMeasure = now;
-
-    getValues();
-    printValues();
-
-    static char temperatureTemp[7];
-    dtostrf(temperatureC, 6, 2, temperatureTemp);
-
-    static char humidityTemp[7];
-    dtostrf(humidity, 6, 2, humidityTemp);
-
-    // Publishes Temperature and Humidity values
-    client.publish("temperatura", temperatureTemp);
-    client.publish("humedad", humidityTemp);
-
-  }
+    client.connect(esp_name);
 
 }
